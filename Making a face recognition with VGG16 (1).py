087742063ab7cace92dd8a61dba970047f0f5e5a
dipeshgpt <64486140+dipeshgpt@users.noreply.github.com>
2020-05-18 22:11:04 +0530

    face recognition

---
+++ Making a face recognition with VGG16 (1).py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Making a Face Recognition with VGG16
+# 
+# ### Loading the VGG16 Model
+
+# In[2]:
+
+
+from keras.applications import VGG16
+
+# VGG16 was designed to work on 224 x 224 pixel input images sizes
+img_rows = 224
+img_cols = 224 
+
+#Loads the VGG16 model 
+model = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+
+# ### Inpsecting each layer
+
+# In[3]:
+
+
+# Let's print our layers 
+for (i,layer) in enumerate(model.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+
+
+# ### Let's freeze all layers except the top 4 
+
+# In[4]:
+
+
+from keras.applications import VGG16
+
+# VGG16 was designed to work on 224 x 224 pixel input images sizes
+img_rows = 224
+img_cols = 224 
+
+# Re-loads the VGG16 model without the top or FC layers
+model = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+# Here we freeze the last 4 layers 
+# Layers are set to trainable as True by default
+for layer in model.layers:
+    layer.trainable = False
+    
+# Let's print our layers 
+for (i,layer) in enumerate(model.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+
+
+# ### Let's make a function that returns our FC Head
+
+# In[6]:
+
+
+def addTopModel(bottom_model, num_classes, D=256):
+    """creates the top or head of the model that will be 
+    placed ontop of the bottom layers"""
+    top_model = bottom_model.output
+    top_model = Flatten(name = "flatten")(top_model)
+    top_model = Dense(D, activation = "relu")(top_model)
+    top_model = Dropout(0.3)(top_model)
+    top_model = Dense(num_classes, activation = "softmax")(top_model)
+    return top_model
+
+
+# In[7]:
+
+
+model.input
+
+
+# In[8]:
+
+
+model.layers
+
+
+# ### Let's add our FC Head back onto VGG
+
+# In[12]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+
+num_classes = 3
+
+FC_Head = addTopModel(model, num_classes)
+
+modelnew = Model(inputs=model.input, outputs=FC_Head)
+
+print(modelnew.summary())
+
+
+# In[ ]:
+
+
+
+
+
+# ### Loading our Faces Dataset
+
+# In[13]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = 'faces/train/'
+validation_data_dir = 'faces/validation/'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Change the batchsize according to your system RAM
+train_batchsize = 16
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+
+# ### Training our top layers
+
+# In[14]:
+
+
+from keras.optimizers import RMSprop
+
+# Note we use a very small learning rate 
+modelnew.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1190
+nb_validation_samples = 170
+epochs = 3
+batch_size = 16
+
+history = modelnew.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+modelnew.save("faces_vgg.h5")
+
+
+# In[ ]:
+
+
+from keras.models import load_model
+
+classifier = load_model('monkey_breed_mobileNet.h5')
+
+
+# In[19]:
+
+
+import os
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+faces_dict = {"[0]": "dipesh", 
+              "[1]": "komal",
+              "[2]": "mahek"}
+
+faces_dict_n = {"dipesh": "dipesh", 
+                "komal": "komal",
+                "mahek": "mahek"}
+
+def draw_test(name, pred, im):
+    faces = faces_dict[str(pred)]
+    BLACK = [0,0,0]
+    expanded_image = cv2.copyMakeBorder(im, 80, 0, 0, 100 ,cv2.BORDER_CONSTANT,value=BLACK)
+    cv2.putText(expanded_image, faces, (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2)
+    cv2.imshow(name, expanded_image)
+
+def getRandomImage(path):
+    """function loads a random images from a random folder in our test path """
+    folders = list(filter(lambda x: os.path.isdir(os.path.join(path, x)), os.listdir(path)))
+    random_directory = np.random.randint(0,len(folders))
+    path_class = folders[random_directory]
+    print("Class - " + faces_dict_n[str(path_class)])
+    file_path = path + path_class
+    file_names = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    random_file_index = np.random.randint(0,len(file_names))
+    image_name = file_names[random_file_index]
+    return cv2.imread(file_path+"/"+image_name)    
+
+for i in range(0,10):
+    input_im = getRandomImage("faces/validation/")
+    input_original = input_im.copy()
+    input_original = cv2.resize(input_original, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
+    
+    input_im = cv2.resize(input_im, (224, 224), interpolation = cv2.INTER_LINEAR)
+    input_im = input_im / 255.
+    input_im = input_im.reshape(1,224,224,3) 
+    
+    # Get Prediction
+    res = np.argmax(modelnew.predict(input_im, 1, verbose = 0), axis=1)
+    
+    # Show image with predicted class
+    draw_test("Prediction", res, input_original) 
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+
+# ## Can we speed this up?
+# #### Let's try re-sizing the image to 64 x 64
+
+# In[ ]:
+
+
+
+
+
+# ### Let's create our new model using an image size of 64 x 64
+
+# In[ ]:
+
+
+
+
+
+# ### Training using 64 x 64 image size is MUCH faster!
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
